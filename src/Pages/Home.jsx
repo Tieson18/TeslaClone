2962b843c394a1ae294e24b8b8e8863b54183b2b
Tieson18 <tieson360@gmail.com>
2024-01-10 12:01:30 +0100

    commited

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,20 +9,20 @@ import SolarR from "../assets/solar-roof.jpg"
 import Accessories from "../assets/accessories.jpg"
 
 
-const Home = () =>
+const Home = () => {
     return (
-    <Box>
-        <Stack>
-            <Section bgImg={ModelS} title={"Model S"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
-            <Section bgImg={ModelY} title={"Model Y"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
-            <Section bgImg={Model3} title={"Model 3"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
-            <Section bgImg={ModelX} title={"Model X"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
-            <Section bgImg={SolarP} title={"Lowest Cost Solar Panels in America"} description={"Money-back guarantee"} leftBtn={"Order now"} RigthBtn={"learn more"} />
-            <Section bgImg={SolarR} title={"Solar for New Roofs"} description={"Solar Roof Costs Less Than a New Roofs Plus Solar Panels"} leftBtn={"Order now"} RigthBtn={"learn more"} />
-            <Section bgImg={Accessories} title={"Accessories"} description={""} leftBtn={"Order now"} />
-        </Stack>
-    </Box>
-)
+        <Box>
+            <Stack>
+                <Section bgImg={ModelS} title={"Model S"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
+                <Section bgImg={ModelY} title={"Model Y"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
+                <Section bgImg={Model3} title={"Model 3"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
+                <Section bgImg={ModelX} title={"Model X"} description={"Order Online for "} underlined={"Touchless Delivery"} leftBtn={"Custom Order"} RigthBtn={"Existing Inventory"} />
+                <Section bgImg={SolarP} title={"Lowest Cost Solar Panels in America"} description={"Money-back guarantee"} leftBtn={"Order now"} RigthBtn={"learn more"} />
+                <Section bgImg={SolarR} title={"Solar for New Roofs"} description={"Solar Roof Costs Less Than a New Roofs Plus Solar Panels"} leftBtn={"Order now"} RigthBtn={"learn more"} />
+                <Section bgImg={Accessories} title={"Accessories"} description={""} leftBtn={"Order now"} />
+            </Stack>
+        </Box>
+    )
 }
 
 export default Home
